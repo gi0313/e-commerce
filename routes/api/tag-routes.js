5f726f13e70bc39726d63d57a45792b9d7f39c48
Giovanni Claro <gio.claro316@gmail.com>
2022-07-20 16:21:15 -0400

    cloned and added columns and routes

---
+++ routes/api/tag-routes.js
@@ -0,0 +1,65 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', (req, res) => {
+  // find all tags
+  // be sure to include its associated Product data
+Tag.findAll({
+  attributes: ['id', 'tag_name'],
+  include: [{
+    model: Product,
+    attributes: ['id', 'product_name', 'price', 'stock'],
+    through: ProductTag,
+    as: 'product'
+  }]
+})
+});
+
+router.get('/:id', (req, res) => {
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+Tag.findOne({
+  where: {
+    id: req.params.id
+  },
+  attributes: ['id', 'tag_name'],
+  include: [{
+    model: Product,
+    include: ['id', 'product_name', 'price', 'stock'],
+    through: ProductTag,
+    as: 'product'
+  }]
+})
+});
+
+router.post('/', (req, res) => {
+  // create a new tag
+Tag.create({
+  tag_name: req.body.tag_name
+})
+});
+
+router.put('/:id', (req, res) => {
+  // update a tag's name by its `id` value
+Tag.update({
+  tag_name: req.body.tag_name
+},
+{
+  where: {
+    id: req.params.id
+  }
+})
+});
+
+router.delete('/:id', (req, res) => {
+  // delete on tag by its `id` value
+Tag.destroy({
+  where: {
+    id: req.params.id
+  }
+})
+});
+
+module.exports = router;
